c04b90689269ae2b707794e0d4e081ceb743f928
Михаил Присяжный <myuprisyazhnyy@edu.hse.ru>
2018-09-18 11:13:50 +0300

    Sem1

--- Sem1/Program.cs
+++ Sem1/Program.cs
@@ -10,6 +10,13 @@ namespace Sem1
     {
         static void Main(string[] args)
         {
+            double b = (1 + Math.Sqrt(5)) / 2;
+            string str = Console.ReadLine();
+            double n;
+           double.TryParse(str, out n);
+            n = (Math.Pow(b, n) - Math.Pow(-b, -n)) / (2 * b - 1);
+            Console.WriteLine(n);
+            Console.ReadKey();
         }
     }
 }
